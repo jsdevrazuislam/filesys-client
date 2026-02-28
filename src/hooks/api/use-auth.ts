3fca15d887b59288e0b702fd508f8821ccfd4be1
jsdevrazuislam <razuislam.upwork@gmail.com>
2026-02-28 20:33:19 +0600

    fix: client side issue

--- src/hooks/api/use-auth.ts
+++ src/hooks/api/use-auth.ts
@@ -29,6 +29,8 @@ export const useLogin = () => {
 
       // 1. Invalidate and await auth queries
       await queryClient.invalidateQueries({ queryKey: ["auth-user"] });
+      Cookies.set("has_session", "true");
+      Cookies.set("user_role", data.user.role);
 
       // 2. Small delay to ensure cookies (has_session, user_role) are persisted by the browser
       // before the Next.js router triggers middleware/server-side checks
